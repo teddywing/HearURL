77837552075762e94b9c8899d43e2f3857bd052e
Teddy Wing <git@teddywing.com>
2017-03-16 04:34:42 +0100

    main.rs: Copy UdpSocket example to test out sockets
    Copies the example given at
    https://doc.rust-lang.org/std/net/struct.UdpSocket.html to experiment
    with receiving and sending data over the socket.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::net::UdpSocket;
+use std::io;
+
+fn make_socket() -> io::Result<()> {
+    let mut socket = try!(UdpSocket::bind("127.0.0.1:34254"));
+
+    // read from the socket
+    let mut buf = [0; 10];
+    let (amt, src) = try!(socket.recv_from(&mut buf));
+
+    // send a reply to the socket we received data from
+    let buf = &mut buf[..amt];
+    buf.reverse();
+    try!(socket.send_to(buf, &src));
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    make_socket();
 }
