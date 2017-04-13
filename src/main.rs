22be6a59a676f85a767d7421a980b965cdcdea35
Teddy Wing <git@teddywing.com>
2017-04-13 02:18:11 +0200

    main.rs: Get rid of UDP code
    Going with TCP as that seems like it will work well. Get rid of our UDP
    code.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,6 @@
-use std::net::UdpSocket;
 use std::io;
-
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
-
-fn make_socket() -> io::Result<()> {
-    let mut socket = try!(UdpSocket::bind("127.0.0.1:34254"));
-
-    // read from the socket
-    let mut buf = [0; 10];
-    let (amt, src) = try!(socket.recv_from(&mut buf));
-
-    // send a reply to the socket we received data from
-    let buf = &mut buf[..amt];
-    buf.reverse();
-    try!(socket.send_to(buf, &src));
-
-    Ok(())
-}
 
 fn open_stream() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:34254")?;
@@ -38,6 +21,5 @@ fn open_stream() -> io::Result<()> {
 }
 
 fn main() {
-    // make_socket();
     open_stream();
 }
