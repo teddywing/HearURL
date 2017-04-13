bc399711f3f31a1a16a03f2440f966dae16ae8e3
Teddy Wing <git@teddywing.com>
2017-04-13 22:16:37 +0200

    open_url.sh: Add documentation comment for Irssi openurl integration

--- open_url.sh
+++ open_url.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 # Opens a URL in the local SSH client machine's browser
+#
+# Irssi openurl.pl configuration:
+#   /set openurl_app_http /home/user/path/to/open_url.sh "$1"
 
 cat <(echo "$1") > /dev/tcp/localhost/37705
